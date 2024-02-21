c5b6d40e5d653759c165880a8f200d8447f5c277
smilewithkhushi <khushipanwar26@gmail.com>
2024-02-21 18:15:35 +0530

    [added] sections  & slider

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
+import BadaltaBharat from "@/components/BadaltaBharat";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Pledge from "@/components/Pledge";
+import Schemes from "@/components/Schemes";
+import StudentCreation from "@/components/StudentCreation";
 import Glimpse from "@/components/glimpse";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-4">
-    
-    <Glimpse />
+      <Header />
+      <Glimpse />
+      <BadaltaBharat />
+      <StudentCreation />
+      <Pledge/>
+      <Schemes/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import next from "next";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div 
+    className="flex w-full align-text-middle 
+    px-2 my-10 justify-around 
+    flex-row flex-nowrap">
+
+      {/* first div containing two images aligned on left of the header */}
+      
+      <div className="flex flex-row gap-2">
+        <Image
+          src="/assets/rajgurulogo.png"
+          width={80}
+          height={80}
+          alt="Shaheed Rajguru college logo"
+        />
+
+        <Image
+          src="/assets/NSSLogo.png"
+          width={80}
+          height={80}
+          alt="NSS logo"
+        />
+      </div>
+        
+        {/* second div containing the heading */} 
+      <h2
+        className="h-min mx-4 px-4 py-2
+        border-2 shadow-sm border-white rounded-3xl
+        text-white text-center
+        md:text-2xl md:px-16 md:py-3
+     hover:shadow-md hover:shadow-slate-200 hover:transition-duration-1000"
+      >
+        <a href="">Click here to submit your idea</a>
+      </h2>
+      <Image
+          src="/assets/viksitbharatlogo.png"
+          width={120}
+          height={80}
+          alt="Viksit Bharat logo"
+        />
+    </div>
+  );
+};
+
+export default Header;
